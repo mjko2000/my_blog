e45c08096e7bdb6fd6be5740ed0ebaaf4835d25d
ndeva <ndeva@Ndevas-Macbook.local>
2021-06-23 00:52:03 +0700

    cm

--- web/src/components/post/PostContent.tsx
+++ web/src/components/post/PostContent.tsx
@@ -8,7 +8,7 @@ function PostContent(props: Props) {
   return (
     <div className = 'w-full px-12 md:px-28 text-justify py-12 dark:text-gray-400'>
       <p className = 'font-bold text-2xl text-center mb-12'>{props.title}</p>
-      <p dangerouslySetInnerHTML = {{__html: props.content}} />
+      <p dangerouslySetInnerHTML = {{__html: props.content ? props.content : ""}} />
       <p className = 'w-full text-right text-sm text-gray-400 mt-8'>By Ndeva, {new Date().toDateString()}</p>
     </div>
   )
