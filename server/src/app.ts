4887e594323af85ca4174143c424f44ac062c8f0
ndeva <ndeva@Ndevas-Macbook.local>
2021-06-25 11:27:10 +0700

    cm

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
 import express, { Request, Response } from "express";
+import LRUCache from 'lru-cache'
 import next from 'next'
 import routes from './routes'
 dotenv.config({path: '../.env'})
@@ -11,10 +12,59 @@ const handleNext = nextApp.getRequestHandler()
 const app = express();
 const port = process.env.PORT || 5055;
 
+const ssrCache = new LRUCache({
+    max: 100 * 1024 * 1024, /* cache size will be 100 MB using `return n.length` as length() function */
+    length: function (n:any, key:any) {
+        return n * 2 + key.length
+    },
+    maxAge: 1000 * 60 * 60 * 24 * 30
+});
+
 app.use('/api',routes)
+app.get('/_next/*', (req, res) => {
+    /* serving _next static content using next.js handler */
+    handleNext(req, res);
+});
 app.get('*', (req, res) => {
-    return handleNext(req, res)
+    // return handleNext(req, res)
+    return renderAndCache(req, res)
 })
+function getCacheKey(req: Request) {
+    return `${req.path}`
+}
+
+async function renderAndCache(req: any, res: Response) {
+    const key = getCacheKey(req);
+
+    // If we have a page in the cache, let's serve it
+    if (ssrCache.has(key)) {
+        console.log(`serving from cache ${key}`);
+        res.setHeader('x-cache', 'HIT');
+        console.log(ssrCache.get(key))
+        res.send(ssrCache.get(key));
+        return
+    }
+
+    try {
+        console.log(`key ${key} not found, rendering`);
+        res.setHeader('x-cache', 'MISS');
+        const html = await nextApp.renderToHTML(req, res, req.path, req.query);
+        console.log(html)
+        // // Something is wrong with the request, let's skip the cache
+        if (res.statusCode !== 200) {
+            res.send(html);
+            return
+        }
+
+        // Let's cache this page
+        ssrCache.set(key, html);
+
+        // res.send(html)
+    } catch (err) {
+        console.log(err)
+        nextApp.renderError(err, req, res, req.path, req.query)
+    }
+}
 nextApp.prepare()
 
 
--- server/src/routes/topic.ts
+++ server/src/routes/topic.ts
@@ -1,38 +1,11 @@
 import { Request, Response, Router } from "express";
 const router = Router();
 
-router.get('/getListTopic',(req,res) => {
-  res.status(200).json({
-    resultCode: 1,
-    data: [
-      {
-        id: 0,
-        title: "First topic",
-        url: "love",
-        thumbnailUrl: `https://picsum.photos/id/${6}/3000/1600`
-      },
-      {
-        id: 1,
-        title: "Second topic",
-        url: "hate",
-        thumbnailUrl: `https://picsum.photos/id/${7}/3000/1600`
-      },
-      {
-        id: 2,
-        title: "Third topic",
-        url: "boo",
-        thumbnailUrl: `https://picsum.photos/id/${8}/3000/1600`
-      }
-    ]
-  })
-})
-
-router.get('/:topic',async (req,res) => {
-  const {topic} = req.params
-  const posts: any = []
+router.get('/getListTopic', async (req,res) => {
+  const listPost: any = []
   const rand = Math.floor(Math.random()*10)
   for (let i = 0; i < 11; i++) {
-    posts.push({
+    listPost.push({
       id: i.toString(),
       title: await fetch("http://metaphorpsum.com/sentences/"+rand).then(reponse => reponse.text()),
       content: await fetch("http://metaphorpsum.com/paragraphs/"+rand).then(reponse => reponse.text()),
@@ -41,12 +14,52 @@ router.get('/:topic',async (req,res) => {
   }
   res.status(200).json({
     resultCode: 1,
+    data: [
+      {
+        id: 0,
+        title: "First topic",
+        url: "love",
+        thumbnailUrl: `https://picsum.photos/id/${6}/3000/1600`,
+        listPost
+      },
+      {
+        id: 1,
+        title: "Second topic",
+        url: "hate",
+        thumbnailUrl: `https://picsum.photos/id/${7}/3000/1600`,
+        listPost
+      },
+      {
+        id: 2,
+        title: "Third topic",
+        url: "boo",
+        thumbnailUrl: `https://picsum.photos/id/${8}/3000/1600`,
+        listPost
+      }
+    ]
+  })
+})
+
+router.get('/:topic',async (req,res) => {
+  const listPost: any = []
+  const rand = Math.floor(Math.random()*10)
+  for (let i = 0; i < 11; i++) {
+    listPost.push({
+      id: i.toString(),
+      title: await fetch("http://metaphorpsum.com/sentences/"+rand).then(reponse => reponse.text()),
+      content: await fetch("http://metaphorpsum.com/paragraphs/"+rand).then(reponse => reponse.text()),
+      thumbnailUrl: `https://picsum.photos/id/${i+rand}/1000/600`
+    })
+  }
+  const {topic} = req.params
+  res.status(200).json({
+    resultCode: 1,
     data: {
       id: 0,
       title: topic,
       url: "love",
       thumbnailUrl: `https://picsum.photos/id/${12}/3000/1600`,
-      posts
+      listPost
     }
   })
 })
