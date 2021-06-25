3df72377b1bd1a4294e0b0691f10ccaa69cfcdcc
ndeva <ndeva@Ndevas-Macbook.local>
2021-06-25 17:18:17 +0700

    cm

--- web/pages/home.tsx
+++ web/pages/home.tsx
@@ -39,7 +39,7 @@ const Home = ({listTopic}: HomeProps) => {
         <p className='text-white text-2xl md:text-4xl font-bold p-2'
         >Welcome to Ndeva blog</p>
         <div
-          onClick={() => { scrollToRef.current.scrollIntoView() }}
+          onClick={() => { scrollToRef.current.scrollIntoView({behavior: 'smooth'}) }}
           className='py-2 px-8 box-border border-2 cursor-pointer md:transition duration-500 md:transform md:hover:scale-110 md:hover:bg-white text-white md:hover:text-gray-700'
         >
           <p className='text-lg'>READ MORE</p>
